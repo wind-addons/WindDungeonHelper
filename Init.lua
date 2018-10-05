51788411388b63503521db70bce0252e65baf726
Zhou Fang <houshuu@live.jp>
2018-10-05 20:13:01 +0900

    change code structure

--- Init.lua
+++ Init.lua
@@ -13,8 +13,7 @@ WDH.L, WDH.LDB = L, LDB
 WDH.Base = {}
 WDH.Config = {}
 WDH.DataBase = {}
-local B = WDH.Base
-local DB = WDH.DataBase
+local B, C, DB = WDH.Base, WDH.Config, WDH.DataBase
 --------------------------------------------------------------
 -- Base
 B.AddonName = L["Wind Dungeon Helper"]
@@ -129,4 +128,30 @@ end
 
 function table.pack(...)
 	return { n = select("#", ...), ... }
+end
+
+function WDH:OnInitialize()
+    DB.profile = LibStub("AceDB-3.0"):New(AddOnName.."DB", DB.defaults).profile
+    C_ChatInfo.RegisterAddonMessagePrefix(B.AddonMsgPrefix)
+    self:SetUpConfig()
+    self:RegisterChatCommand("wdh", "OpenOptionFrame")
+end
+
+function WDH:SetUpConfig()
+    LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable(AddOnName, function() return C.CreateOptionTable() end)
+    self.optionFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(AddOnName, B.AddonName)
+    
+    local logo = CreateFrame('Frame', nil, self.optionFrame)
+    logo:SetFrameLevel(4)
+    logo:SetSize(64, 64)
+    logo:SetPoint('TOPRIGHT', -12, -12)
+    logo:SetBackdrop({bgFile = ('Interface\\ICONS\\Achievement_Dungeon_Mythic15'):format(AddOnName)})
+    self.optionFrame.logo = logo
+
+    LibStub("LibDBIcon-1.0"):Register(L["Wind Dungeon Helper"], self.LDB, DB.profile.minimapicon)
+end
+
+function WDH:OpenOptionFrame()
+    InterfaceOptionsFrame_OpenToCategory(self.optionFrame)
+    InterfaceOptionsFrame_OpenToCategory(self.optionFrame)
 end
