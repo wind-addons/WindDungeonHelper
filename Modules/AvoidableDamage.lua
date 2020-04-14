e5429b21088bcd46b16f75887667ffd1ab39424b
Zhou Fang <fang2hou@outlook.com>
2020-04-14 21:11:50 +0900

    update spells and auras

--- Modules/AvoidableDamage.lua
+++ Modules/AvoidableDamage.lua
@@ -311,6 +311,12 @@ local Spells = {
     [276286] = 20,    --- Slicing Hurricane (Environment)
     [276292] = 20,    --- Whirlign Slam (Ironhull Apprentice)
     [267385] = 20,    --- Tentacle Slam (Vol'zith the Whisperer)
+    -- import from ElitismHelper
+    [276268] = 20,		--- Heaving Blow (Shrine Templar)
+	[267973] = 20,		--- Wash Away (Temple Attendant)
+	[268059] = 20,		--- Anchor of Binding (Tidesage Spiritualist)
+	[264155] = 20,		--- Surging Rush (Aqu'sirr)
+	[267841] = 20,		--- Blowback (Galecaller Faye)
 
     -- Siege of Boralus
     [256627] = 20,    --- Slobber Knocker (Scrimshaw Enforcer)
@@ -324,6 +330,16 @@ local Spells = {
     [257292] = 20,    --- Heavy Slash (Irontide Cleaver)
     [273681] = 20,    --- Heavy Hitter (Chopper Redhook)
     [257886] = 20,    --- Brine Pool (Hadal Darkfathom)
+    -- import from ElitismHelper
+	[257069] = 20,		--- Watertight Shell (Kul Tiran Wavetender)
+	[268260] = 20,		--- Broadside (Ashvane Cannoneer)
+	[268443] = 20,		--- Dread Volley (Dread Captain Lockwood)
+	[272713] = 20,		--- Crushing Slam (Bilge Rat Demolisher)
+	[274941] = 20,		--- Banana Rampage swirlies(Bilge Rat Buccaneer)
+	[257883] = 20,		--- Break Water (Hadal Darkfathom)
+	[276068] = 20,		--- Tidal Surge (Hadal Darkfathom)
+	[261565] = 20,		--- Crashing Tide (Hadal Darkfathom)
+	[277535] = 20,		--- Viq'Goth's Wrath (Viq'Goth)
 
     -- Tol Dagor
     [257785] = 20,    --- Flashing Daggers
@@ -334,7 +350,10 @@ local Spells = {
     [258864] = 20,    --- Suppression Fire (Ashvane Marine/Spotter)
     [258364] = 20,    --- Fuselighter (Ashvane Flamecaster)
     [259711] = 20,    --- Lockdown (Ashvane Warden)
-
+    -- import from ElitismHelper
+	[257119] = 20,		--- Sand Trap (The Sand Queen)
+	[256710] = 20,		--- Burning Arsenal (Knight Captain Valyri)
+	
     -- Waycrest Manor
     [260569] = 20,    --- Wildfire (Soulbound Goliath)
     [265407] = 20,    --- Dinner Bell (Banquet Steward)
@@ -343,6 +362,14 @@ local Spells = {
     [271174] = 20,    --- Retch (Pallid Gorger)
     [268387] = 20,    --- Contagious Remnants (Lord Waycrest)
     [268308] = 20,    --- Discordant Cadenza (Lady Waycrest
+    -- import from ElitismHelper
+    [264531] = 20,		--- Shrapnel Trap (Maddened Survivalist)
+	[264476] = 20,		--- Tracking Explosive (Crazed Marksman)
+	[264712] = 20,		--- Rotten Expulsion (Raal the Gluttonous)
+	[272669] = 20,		--- Burning Fists (Soulbound Goliath)
+	[278849] = 20,		--- Uproot (Coven Thornshaper)
+	[264040] = 20,		--- Uprooted Thorns (Coven Thornshaper)
+	[265757] = 20,		--- Splinter Spike (Matron Bryndle)
 
     -- Atal'Dazar
     [253666] = 20,    --- Fiery Bolt (Dazar'ai Juggernaught)
@@ -353,7 +380,10 @@ local Spells = {
     [250022] = 20,    --- Echoes of Shadra (Echoes of Shadra)
     [250585] = 20,    --- Toxic Pool
     [250036] = 20,    --- Shadowy Remains
-
+    -- import from ElitismHelper
+    [258723] = 20,		--- Grotesque Pool (Renaimated Honor Guard)
+    [255567] = 20,		--- Frenzied Charge (T'lonja)
+    
     -- King's Rest
     [265914] = 20,    --- Molten Gold (The Golden Serpent)
     [266191] = 20,    --- Whirling Axe (Council of Tribes)
@@ -363,6 +393,16 @@ local Spells = {
     [270485] = 20,    --- Blooded Leap (Spectral Berserker)
     [267639] = 20,    --- Burn Corruption (Mchimba the Embalmer)
     [270931] = 20,    --- Darkshot
+    -- import from ElitismHelper
+    [270003] = 20,		--- Suppression Slam (Animated Guardian)
+	[269932] = 20,		--- Ghust Slash (Shadow-Borne Warrior)
+	[265781] = 20,		--- Serpentine Gust (The Golden Serpent)
+	[270928] = 20,		--- Bladestorm (King Timalji)
+	[270891] = 20,		--- Channel Lightning (King Rahu'ai)
+	[270292] = 20,		--- Purifying Flame (Purification Construct)
+	[270514] = 20,		--- Ground Crush (Spectral Brute)
+	[268419] = 20,		--- Gale Slash (King Dazar)
+	[268796] = 20,		--- Impaling Spear (King Dazar)
 
     -- The MOTHERLODE!!
     [257371] = 20,    --- Gas Can (Mechanized Peace Keeper)
@@ -379,6 +419,12 @@ local Spells = {
     [262348] = 20,    --- Mine Blast
     [257337] = 20,    --- Shocking Claw
     [269092] = 20,    --- Artillery Barrage (Ordnance Specialist)
+    -- import from ElitismHelper
+    [268417] = 20,		--- Power Through (Azerite Extractor)
+	[268704] = 20,		--- Furious Quake (Stonefury)
+	[258628] = 20,		--- Resonant Quake (Earthrager)
+	[271583] = 20,		--- Black Powder Special (Mines near the track)
+	[269831] = 20,		--- Toxic Sludge (Oil Environment)
 
     -- Temple of Sethraliss
     [268851] = 20,    --- Lightning Shield (Adderis)
@@ -389,6 +435,13 @@ local Spells = {
     [272657] = 20,    --- Noxious Breath
     [272658] = 20,    --- Electrified Scales
     [272821] = 20,    --- Call Lightning (Stormcaller)
+    -- import from ElitismHelper
+    [263425] = 20,		--- Arc Dash (Adderis)
+	[263573] = 20,		--- Cyclone Strike (Adderis)
+	[272655] = 20,		--- Scouring Sand (Mature Krolusk)
+	[272696] = 20,		--- Lightning in a Bottle (Crazed Incubator)
+	[264763] = 20,		--- Spark (Static-charged Dervish)
+	[279014] = 20,		--- Cardiac Shock (Avatar, Environment)
 
     -- Underrot
     [264757] = 20,    --- Sanguine Feast (Elder Leaxa)
@@ -399,30 +452,99 @@ local Spells = {
     [265511] = 20,    --- Spirit Drain (Spirit Drain Totem)
     [272469] = 20,    --- Abyssal Slam (Abyssal Reach)
     [272609] = 20,    --- Maddening Gaze (Faceless Corruptor)
+    -- import from ElitismHelper
+    [260312] = 20,		--- Charge (Cragmaw the Infested)
+	[259720] = 20,		--- Upheaval (Sporecaller Zancha)
+	[270108] = 20,		--- Rotting Spore (Unbound Abomination)
+    [270108] = 20,		--- Rotting Spore (Unbound Abomination)
+    
+    -- from ElitismHelper and https://nga.178.com/read.php?&tid=15265896&pid=396636585&to=1
+    -- Mechagon Workshop
+	[294128] = 20,		--- Rocket Barrage (Rocket Tonk)
+	[285020] = 20,		--- Whirling Edge (The Platinum Pummeler)
+	[294291] = 20,		--- Process Waste ()
+	[291930] = 20,		--- Air Drop (K.U-J.0)
+	[294324] = 20,		--- Mega Drill (Waste Processing Unit)
+	[293861] = 20,		--- Anti-Personnel Squirrel (Anti-Personnel Squirrel)
+	[295168] = 20,		--- Capacitor Discharge (Blastatron X-80)
+    [294954] = 20,		--- Self-Trimming Hedge (Head Machinist Sparkflux)
+    [283422] = 20,    -- 车+机器人，全速前进
+    [282945] = 20,    -- 车+机器人，电锯
+    [285344] = 20,    -- 车+机器人，埋设地雷
+    [291953] = 20,    -- 狂犬，垃圾炸弹
+    [291946] = 20,    -- 狂犬，喷射烈焰
+    [301299] = 20,    -- 传送带，焚炉烈焰
+    [294015] = 20,    -- 爆破金刚，发射高爆火箭
+    [293986] = 20,    -- 爆破金刚，声波脉冲
+    [285454] = 20,    -- 机械师闪流，脉冲榴弹
+    [291915] = 20,    -- 国王，等离子球
+    [291856] = 20,    -- 国王，离子校正
+	
+    -- Mechagon Junkyard
+    -- from ElitismHelper and https://nga.178.com/read.php?&tid=15265896&pid=396636585&to=1
+	[300816] = 20,		--- Slimewave (Slime Elemental)
+	[300188] = 20,		--- Scrap Cannon (Weaponized Crawler)
+	[300427] = 20,		--- Shockwave (Scrapbone Bully)
+	[294890] = 20,		--- Gryro-Scrap (Malfunctioning Scrapbot)
+	[300129] = 20,		--- Self-Destruct Protocol (Malfunctioning Scrapbot)
+	[300561] = 20,		--- Explosion (Scrapbone Trashtosser)
+	[299475] = 20,		--- B.O.R.K. (Scraphound)
+	[299535] = 20,		--- Scrap Blast (Pistonhead Blaster)
+	[298940] = 20,		--- Bolt Buster (Naeno Megacrash)
+    [297283] = 20,		--- Cave In (King Gobbamak)
+    [300159] = 20,    -- 重装拳机，旋翼风暴
+    [297835] = 20,    -- 冈克，融合
+    [297985] = 20,    -- 冈克，泼溅
+    [297834] = 20,    -- 冈克，剧毒波浪
+    [299164] = 20,    -- 男女，风驰电掣
+    [302681] = 20,    -- 男女，超能跳电
+    [298849] = 20,    -- 男女，电流滑步
+    [298571] = 20,    -- 男女，燃尽
+    [301667] = 20,    -- 麦卡贡骑兵，急速射击
+    [295536] = 20,    -- 飞机，火炮冲击
+    [302384] = 20,    -- 飞机，静电释放
+    [296522] = 20,    -- 飞机，自毁
+    [296150] = 20,    -- 飞机，喷涌冲击
+	
+    --- Awakened Lieutenant
+    [240448] = 20,    -- 震荡
+    [314387] = 20,    -- 秒t怪菌毯
+	[314309] = 20,		--- Dark Fury (Urg'roth, Breaker of Heroes)
+	[314467] = 20,		--- Volatile Rupture (Voidweaver Mal'thir)
+	[314565] = 20,		--- Defiled Ground (Blood of the Corruptor)
 }
 local SpellsNoTank = {
     -- Freehold
 
-    -- Shrine of the Storm
-    [267899] = 20,      --- Hindering Cleave
+	-- Shrine of the Storm
+	
+	-- Siege of Boralus
+	[268230] = 20,		--- Crimson Swipe (Ashvane Deckhand)
+	
+	-- Tol Dagor
+	[258864] = 20,		--- Suppression Fire (Ashvane Marine/Spotter)
+	
+	-- Waycrest Manor
+	[263905] = 20,		--- Marking Cleave (Heartsbane Runeweaver)
+	[265372] = 20,		---	Shadow Cleave (Enthralled Guard)
+	[271174] = 20,		--- Retch (Pallid Gorger)
+	
+	-- Atal'Dazar
 
-    -- Siege of Boralus
+	-- King's Rest
+	[270289] = 20,		--- Purification Beam (Purification Construct)
+	
+	-- The MOTHERLODE!!
+	[268846] = 20,		--- Echo Blade (Weapons Tester)
+	[263105] = 20,		--- Blowtorch (Feckless Assistant)
+	[263583] = 20,		--- Broad Slash (Taskmaster Askari)
+		
+	-- Temple of Sethraliss
+	[255741] = 20,		--- Cleave (Scaled Krolusk Rider)
 
-    -- Tol Dagor
-
-    -- Waycrest Manor
-
-    -- Atal'Dazar
-
-    -- King's Rest
-
-    -- The MOTHERLODE!!
-
-    -- Temple of Sethraliss
-    [255741] = 20,    	--- Cleave (Scaled Krolusk Rider)
-
-    -- Underrot
-    [265019] = 20,    	--- Savage Cleave (Chosen Blood Matron)
+	-- Underrot
+	[272457] = 20,		--- Shockwave (Sporecaller Zancha)
+	[260793] = 20,		--- Indigestion (Cragmaw the Infested)
 }
 local Auras = {
     -- Freehold
@@ -432,10 +554,13 @@ local Auras = {
     -- Shrine of the Storm
     [268391] = true,    -- Mental Assault (Abyssal Cultist)
     [276268] = true,    -- Heaving Blow (Shrine Templar)
+    [269104] = true,		-- Explosive Void (Lord Stormsong)
+	[267956] = true,		-- Zap (Jellyfish)
 
     -- Siege of Boralus
     [257292] = true,    -- Heavy Slash (Kul Tiran Vanguard)
     [272874] = true,    -- Trample (Ashvane Commander)
+    [257169] = true,		-- Fear
 
     -- Tol Dagor
     [257119] = true,    -- Sand Trap (The Sand Queen)
@@ -443,8 +568,10 @@ local Auras = {
 
     -- Waycrest Manor
     [265352] = true,    -- Toad Blight (Toad)
+    [278468] = true,		-- Freezing Trap
 
     -- Atal'Dazar
+    [255371] = true,		-- Terrifying Visage (Rezan)
 
     -- King's Rest
     [270003] = true,    -- Suppression Slam (Animated Guardian)
@@ -460,6 +587,16 @@ local Auras = {
     -- Underrot
     [272609] = true,    -- Maddening Gaze (Faceless Corrupter)
 
+    -- Mechagon Workshop
+    [293986] = true,		--- Sonic Pulse (Blastatron X-80)
+    [294863] = true,   --机械师，烈油之泉
+    [285440] = true,   --机械师，烈焰火炮
+	
+	-- Mechaton Junkyard
+	[398529] = true,		-- Gooped (Gunker)
+    [300659] = true,		-- Consuming Slime (Toxic Monstrosity)
+    [298124] = true,    -- 冈克，束缚粘液
+    [298259] = true,    -- 冈克，束缚粘液
 }
 local AurasNoTank = {
 }
