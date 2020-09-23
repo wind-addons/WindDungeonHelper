7d9daacefbc40dc656d574f6ae917b27238ba36c
Zhou Fang <fang2hou@outlook.com>
2020-09-23 23:52:43 +0900

    Update database building

---
+++ Settings/Core.lua
@@ -0,0 +1,11 @@
+local W, F, L, P, O = unpack(select(2, ...))
+
+P = {
+    enable = true,
+    minimapIcon = true
+}
+
+function W:BuildDatabase()
+    self.Database = LibStub("AceDB-3.0"):New("WindDungeonHelperDB", {profile = P})
+    self.db = self.Database.profile
+end
