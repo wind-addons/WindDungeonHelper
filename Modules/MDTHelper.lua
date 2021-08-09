3e2e77f49f6dda49192d9e5b2e7a42f56559ca49
Zhou Fang <fang2hou@outlook.com>
2021-08-09 09:29:26 +0900

    fix MDT translation

--- Modules/MDTHelper.lua
+++ Modules/MDTHelper.lua
@@ -11,46 +11,26 @@ local ReloadUI = ReloadUI
 
 local C_Timer_After = C_Timer.After
 
-local TypeDict = {
-    ["Humanoid"] = L["Humanoid"],
-    ["Beast"] = L["Beast"],
-    ["Undead"] = L["Undead"],
-    ["Elemental"] = L["Elemental"],
-    ["Aberration"] = L["Aberration"],
-    ["Not specified"] = L["Not specified"],
-    ["Mechanical"] = L["Mechanical"],
-    ["Dragonkin"] = L["Dragonkin"]
-}
-
 function MH:Translate()
     if not self.db or not self.db.enable or not self.db.translate then
         return
     end
 
-    if not _G.MDT or not _G.MDT.dungeonEnemies then
+    local MDT = _G.MDT
+
+    if not MDT or not MDT.dungeonEnemies or not MDT.dungeonBosses or not MDT.L then
         return
     end
 
-    if _G.MDT.L then
-        for _, localeString in pairs(TypeDict) do
-            _G.MDT.L[localeString] = localeString
-        end
-    end
-
-    for mapID, NPCs in pairs(_G.MDT.dungeonEnemies) do
+    for mapID, NPCs in pairs(MDT.dungeonEnemies) do
         if mapID >= 29 and NPCs then
             for _, NPC in pairs(NPCs) do
-                if NPC.id and NPC.name then
-                    local name = F.GetNPCNameByID(NPC.id)
+                if NPC["id"] and NPC["name"] then
+                    local name = F.GetNPCNameByID(NPC["id"])
                     if name then
-                        NPC.name = name
-                        if _G.MDT.L then
-                            _G.MDT.L[name] = name
-                        end
+                        NPC["name"] = name
+                        MDT.L[name] = name
                     end
-                end
-                if NPC.creatureType then
-                    NPC.creatureType = TypeDict[NPC.creatureType] or NPC.creatureType
                 end
             end
         end
@@ -61,7 +41,7 @@ function MH:MDTLoaded()
     if not self.db or not self.db.enable then
         return
     end
-    --self:Translate()
+    self:Translate()
 end
 
 function MH:ProfileUpdate()
