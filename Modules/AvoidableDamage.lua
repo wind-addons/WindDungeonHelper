5dfe47901ba56aa39cf0c99694549a6d00efb036
fang2hou <fang2hou@outlook.com>
2022-08-06 00:55:34 +0900

    add more karazhan lower mistake ids

--- Modules/AvoidableDamage.lua
+++ Modules/AvoidableDamage.lua
@@ -1402,9 +1402,14 @@ local MistakeData = {
     ["Return to Karazhan: Lower"] = {
         -- 小怪
         {
-            -- 喪志打擊 (克利斯平‧費蘭斯大人)
+            -- 哥布林龍槍 (魅影賓客)
             type = MISTAKE.SPELL_DAMAGE,
-            spell = 227672
+            spell = 21910
+        },
+        {
+            -- 琅琅詩詞 (鬼魅候補演員)
+            type = MISTAKE.SPELL_DAMAGE,
+            spell = 227917
         },
         {
             -- 勁爆終曲 (鬼魅候補演員)
@@ -1412,9 +1417,24 @@ local MistakeData = {
             spell = 227925
         },
         {
+            -- 閃光 (骷髏接待員)
+            type = MISTAKE.SPELL_DAMAGE,
+            spell = 227966
+        },
+        {
+            -- 閃光 (骷髏接待員)
+            type = MISTAKE.SPELL_DAMAGE,
+            spell = 227977
+        },
+        {
             -- 乾坤一擲 (鬼魅慈善家)
             type = MISTAKE.SPELL_DAMAGE,
             spell = 228001
+        },
+        {
+            -- 衝鋒 (鬼靈戰騎)
+            type = MISTAKE.SPELL_DAMAGE,
+            spell = 228603
         },
         {
             -- 秘法爆發 (秘法看守者)
@@ -1437,7 +1457,23 @@ local MistakeData = {
             type = MISTAKE.SPELL_DAMAGE,
             spell = 227799
         },
+        -- 歌劇大廳: 美女與野獸
+        {
+            -- 殘羹
+            type = MISTAKE.SPELL_DAMAGE,
+            spell = 228019
+        },
+        {
+            -- 兇猛除塵
+            type = MISTAKE.SPELL_DAMAGE,
+            spell = 228215
+        },
         -- 獵人阿圖曼
+        {
+            -- 苦難共享
+            type = MISTAKE.SPELL_DAMAGE,
+            spell = 228852
+        },
         {
             -- 舉踏
             type = MISTAKE.SPELL_DAMAGE,
@@ -1447,6 +1483,12 @@ local MistakeData = {
             -- 鬼騎衝鋒
             type = MISTAKE.SPELL_DAMAGE,
             spell = 227645
+        },
+        -- 摩洛斯
+        {
+            -- 喪志打擊
+            type = MISTAKE.SPELL_DAMAGE,
+            spell = 227672
         }
     },
     ["Return to Karazhan: Upper"] = {
