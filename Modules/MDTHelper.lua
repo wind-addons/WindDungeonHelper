3353c9ceb98ca50683f77a0bf8eeb27298bd73fa
EXWIND <exw081697@gmail.com>
2024-07-16 18:39:58 +0800

    siege tag

--- Modules/MDTHelper.lua
+++ Modules/MDTHelper.lua
@@ -18,10 +18,10 @@ local currentPatchMaps = {
     [8] = true, -- Neltharion's Lair
     [15] = true, -- The Arcway
     [16] = true, -- Freehold
-    [19] = true, -- Siege of Boralus
+    [19] = true, -- Siege of Boralus (TWW S1)
     [22] = true, -- The Underrot
     [31] = true, -- Mists of Tirna Scithe (TWW S1)
-    [35] = true, -- The NecroticWake (TWW S1)
+    [35] = true, -- The Necrotic Wake (TWW S1)
     [42] = true, -- Ruby Life Pools
     [43] = true, -- The Nokhud Offensive
     [44] = true, -- The Azure Vault
@@ -42,7 +42,7 @@ local currentPatchMaps = {
     [110] = true, -- The Stonevault (TWW S1)
     [111] = true, -- The Dawnbreaker (TWW S1)
     [112] = true, -- Grim Batol (TWW S1)
-    [113] = true, -- Ara-Kara, City of Echoes (TWW S1)
+    [113] = true, -- Ara-Kara,City of Echoes (TWW S1)
     [114] = true -- City of Threads (TWW S1)
 }
 
