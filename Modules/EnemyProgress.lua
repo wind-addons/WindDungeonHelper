3d8e619cd347ef03457d89c9688819d589470178
Zhou Fang <fang2hou@outlook.com>
2024-09-17 10:45:10 +0900

    feat: sync logic from windtools

--- Modules/EnemyProgress.lua
+++ Modules/EnemyProgress.lua
@@ -14,6 +14,9 @@ local TooltipDataProcessor_AddTooltipPostCall = TooltipDataProcessor.AddTooltipP
 
 local Enum_TooltipDataType_Unit = Enum.TooltipDataType.Unit
 
+local icon1 = F.GetIconString(132147, 14)
+local icon2 = F.GetIconString(5926319, 14)
+
 local function isWindToolsLoaded()
 	if C_AddOns_IsAddOnLoaded("ElvUI_WindTools") then
 		local E = _G.ElvUI and _G.ElvUI[1]
@@ -32,23 +35,37 @@ function EP:AddEnemyProgress(tt, data)
 		return
 	end
 
+	local accuracy = self.db.accuracy
 	local npcID = select(6, strsplit("-", data.guid))
 	if not npcID or npcID == "" then
 		return
 	end
 
 	npcID = tonumber(npcID)
+
 	local count, max = _G.MDT:GetEnemyForces(npcID)
 
 	if count and max and count > 0 and max > 0 then
-		local left = format("%s |cff00d1b2%s|r |cffffffff-|r |cffffdd57%s|r", F.GetIconString(132147, 14), count, max)
-		local right = format(
-			"%s |cff209cee%s|r|cffffffff%%|r",
-			F.GetIconString(5926319, 14),
-			F.Round(100 * count / max, self.db.accuracy)
-		)
+		local left = format("%s |cff00d1b2%s|r |cffffffff-|r |cffffdd57%s|r", icon1, count, max)
+		local right = format("%s |cff209cee%s|r|cffffffff%%|r", icon2, F.Round(100 * count / max, accuracy))
+
+		-- If the line already exists, update it
+		if GameTooltip.__windEnemyProgress then
+			for i = 1, tt:NumLines() do
+				local leftTextObj = _G["GameTooltipTextLeft" .. i]
+				local rightTextObj = _G["GameTooltipTextRight" .. i]
+				local leftText = leftTextObj and leftTextObj:GetText()
+				if leftText and rightTextObj and strfind(leftText, "^" .. icon1) then
+					leftTextObj:SetText(left)
+					rightTextObj:SetText(right)
+					return
+				end
+			end
+		end
+
 		GameTooltip:AddDoubleLine(left, right)
 		GameTooltip:Show()
+		GameTooltip.__windEnemyProgress = true
 	end
 end
 
@@ -58,6 +75,10 @@ function EP:ProfileUpdate()
 	if self.db and self.db.enable then
 		TooltipDataProcessor_AddTooltipPostCall(Enum_TooltipDataType_Unit, function(...)
 			self:AddEnemyProgress(...)
+		end)
+
+		GameTooltip:HookScript("OnTooltipCleared", function(tt)
+			tt.__windEnemyProgress = nil
 		end)
 	end
 end
