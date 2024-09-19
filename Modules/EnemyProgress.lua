0b09da3fd6ed3397d06f24c7ed4f89d4391151b3
Zhou Fang <fang2hou@outlook.com>
2024-09-19 09:02:42 +0900

    fix: fix the tooltip

--- Modules/EnemyProgress.lua
+++ Modules/EnemyProgress.lua
@@ -8,6 +8,7 @@ local select = select
 local tonumber = tonumber
 
 local GameTooltip = _G.GameTooltip
+local GetInstanceInfo = GetInstanceInfo
 
 local C_AddOns_IsAddOnLoaded = C_AddOns.IsAddOnLoaded
 local TooltipDataProcessor_AddTooltipPostCall = TooltipDataProcessor.AddTooltipPostCall
@@ -31,7 +32,18 @@ function EP:AddEnemyProgress(tt, data)
 		return
 	end
 
-	if isWindToolsLoaded() or not tt or not tt == _G.GameTooltip and not tt.NumLines or tt:NumLines() == 0 then
+	local difficultyID = select(3, GetInstanceInfo())
+	if not difficultyID or difficultyID ~= 8 then
+		return
+	end
+
+	if
+		isWindToolsLoaded()
+		or not tt
+		or not tt == _G.GameTooltip and not tt.NumLines
+		or tt:NumLines() == 0
+		or tt.__windEnemyProgress
+	then
 		return
 	end
 
@@ -41,31 +53,15 @@ function EP:AddEnemyProgress(tt, data)
 		return
 	end
 
-	npcID = tonumber(npcID)
-
-	local count, max = _G.MDT:GetEnemyForces(npcID)
+	tt.__windEnemyProgress = true
+	local count, max = _G.MDT:GetEnemyForces(tonumber(npcID))
 
 	if count and max and count > 0 and max > 0 then
 		local left = format("%s |cff00d1b2%s|r |cffffffff-|r |cffffdd57%s|r", icon1, count, max)
 		local right = format("%s |cff209cee%s|r|cffffffff%%|r", icon2, F.Round(100 * count / max, accuracy))
 
-		-- If the line already exists, update it
-		if GameTooltip.__windEnemyProgress then
-			for i = 1, tt:NumLines() do
-				local leftTextObj = _G["GameTooltipTextLeft" .. i]
-				local rightTextObj = _G["GameTooltipTextRight" .. i]
-				local leftText = leftTextObj and leftTextObj:GetText()
-				if leftText and rightTextObj and strfind(leftText, "^" .. icon1) then
-					leftTextObj:SetText(left)
-					rightTextObj:SetText(right)
-					return
-				end
-			end
-		end
-
-		GameTooltip:AddDoubleLine(left, right)
-		GameTooltip:Show()
-		GameTooltip.__windEnemyProgress = true
+		tt:AddDoubleLine(left, right)
+		tt:Show()
 	end
 end
 
