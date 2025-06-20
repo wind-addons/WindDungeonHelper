d4a4d3892ce04b557257f3fa2fa067d4df67ab48
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-06-20 19:59:36 +0500

    Prep for Season 3

--- Core/Metadata.lua
+++ Core/Metadata.lua
@@ -15,6 +15,16 @@ W.MythicPlusMapData = {
 	[525] = { abbr = L["[ABBR] Operation: Floodgate"], activityID = 371, timers = { 1188, 1584, 1980 } },
 }
 
+-- The War Within Season 3 Dungeons
+-- [378] = { abbr = L["[ABBR] Halls of Atonement"], activityID = 261, timers = { 1152, 1536, 1920 } },
+-- [391] = { abbr = L["[ABBR] Tazavesh: Streets of Wonder"], activityID = 280, timers = { 1404, 1872, 2340 } },
+-- [392] = { abbr = L["[ABBR] Tazavesh: So'leah's Gambit"], activityID = 281, timers = { 1080, 1440, 1800 } },
+-- [499] = { abbr = L["[ABBR] Priory of the Sacred Flame"], activityID = 324, timers = { 1170, 1560, 1950 } },
+-- [503] = { abbr = L["[ABBR] Ara-Kara, City of Echoes"], activityID = 323, timers = { 1080, 1440, 1800 } },
+-- [505] = { abbr = L["[ABBR] The Dawnbreaker"], activityID = 326, timers = { 1116, 1488, 1860 } },
+-- [525] = { abbr = L["[ABBR] Operation: Floodgate"], activityID = 371, timers = { 1188, 1584, 1980 } },
+-- [542] = { abbr = L["[ABBR] Eco-Dome Al'dani"], activityID = 381, timers = { 1116, 1488, 1860 } },
+
 function W:InitializeMetadata()
 	for id in pairs(W.MythicPlusMapData) do
 		local name, _, timeLimit, tex = C_ChallengeMode_GetMapUIInfo(id)
