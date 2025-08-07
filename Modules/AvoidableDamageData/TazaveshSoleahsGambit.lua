f564f3c6cd008a8f3db7a6572af23fd507a00b46
Zhou Fang <fang2hou@outlook.com>
2025-08-07 22:16:50 +0900

    feat: update gmbt

--- Modules/AvoidableDamageData/TazaveshSoleahsGambit.lua
+++ Modules/AvoidableDamageData/TazaveshSoleahsGambit.lua
@@ -2,38 +2,6 @@ local W, F, L, P = unpack(select(2, ...))
 local AD = W:GetModule("AvoidableDamage")
 
 local mistakes = {
-	-- 小怪
-	{
-		-- 易爆河豚 (暗洋魚法師)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355234,
-	},
-	{
-		-- 巨石投擲 (岸行者巨人)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355465,
-	},
-	{
-		-- 轟雷 (風鑄守護者)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355581,
-	},
-	{
-		-- 充電脈衝 (風鑄守護者)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355584,
-	},
-	{
-		-- 潮汐爆發 (沙漏號浪潮賢者)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 356260,
-	},
-	{
-		-- 漂流之星 (絢麗觀星者)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 357228,
-	},
-	-- [1] 海布藍德
 	{
 		-- 淨滅火焰
 		type = AD.MISTAKE.SPELL_DAMAGE,
@@ -50,12 +18,6 @@ local mistakes = {
 		spell = 347094,
 	},
 	{
-		-- 符文回應
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 356796,
-	},
-	-- [2] 時光船長鉤尾
-	{
 		-- 無限吐息
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 347149,
@@ -71,17 +33,6 @@ local mistakes = {
 		spell = 347370,
 	},
 	{
-		-- 鉤到啦！
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 354334,
-	},
-	{
-		-- 燃燒焦油
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 358947,
-	},
-	-- [3] 索利亞
-	{
 		-- 能量分離
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 351101,
@@ -90,6 +41,51 @@ local mistakes = {
 		-- 極光新星
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 351646,
+	},
+	{
+		-- 鉤到啦！
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 354334,
+	},
+	{
+		-- 易爆河豚
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355234,
+	},
+	{
+		-- 巨石投擲
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355465,
+	},
+	{
+		-- 轟雷
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355581,
+	},
+	{
+		-- 充電脈衝
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355584,
+	},
+	{
+		-- 潮汐爆發
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 356260,
+	},
+	{
+		-- 符文回應
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 356796,
+	},
+	{
+		-- 漂流之星
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 357228,
+	},
+	{
+		-- 燃燒焦油
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 358947,
 	},
 }
 
