0504c8fc17300d1033737a411cf5b500db3cd92a
Zhou Fang <fang2hou@outlook.com>
2025-08-07 21:55:41 +0900

    style: sort with id

--- Modules/AvoidableDamageData/TazaveshStreetsOfWonder.lua
+++ Modules/AvoidableDamageData/TazaveshStreetsOfWonder.lua
@@ -2,90 +2,55 @@ local W, F, L, P = unpack(select(2, ...))
 local AD = W:GetModule("AvoidableDamage")
 
 local mistakes = {
-	-- 小怪
 	{
-		-- 裂隙衝擊 (傳送門法師佐洪)
+		-- 灑出的液體
 		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355306,
+		spell = 346329,
 	},
 	{
-		-- 震擊地雷 (指揮官佐發)
+		-- 殊離之環
 		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355476,
+		spell = 347481,
 	},
-	{
-		-- 致命武力 (指揮官佐發)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355487,
-	},
-	{
-		-- 震光屏障 (環境)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355502,
-	},
-	{
-		-- 鎮壓猛襲 (市場保安官)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355638,
-	},
-	{
-		-- 干擾手榴彈 (海關警衛)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 355903,
-	},
-	{
-		-- 光束接合者 (武裝監督者, 『追蹤者』佐寇司)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 356011,
-	},
-	{
-		-- 腐爛的食物 (脫序的顧客)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 356482,
-	},
-	{
-		-- 聖光裂片撤退 (集團幫派)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 357019,
-	},
-	-- [1] 佐菲克斯
 	{
 		-- 武裝保全
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 348366,
 	},
-	-- [2] 大展示廳
 	{
-		-- 飢餓之握 (阿克魯斯)
+		-- 飢餓之握
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 349663,
 	},
 	{
-		-- 宏偉吞噬 (阿克魯斯)
+		-- 宏偉吞噬
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 349801,
 	},
 	{
-		-- 靈魄引爆 (亞奇力特)
+		-- 靈魄引爆
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 349999,
 	},
 	{
-		-- 散熱震盪 (亞奇力特)
-		-- 測試 (可能無法避免)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 351070,
-	},
-	{
-		-- 旋風之滅 (溫札‧金熔)
+		-- 旋風之滅
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 350090,
 	},
-	-- [3] 佐戈隆
 	{
 		-- 控場
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 350921,
+	},
+	{
+		-- 散熱震盪
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 351070,
+	},
+	{
+		-- 裂隙衝擊
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355306,
 	},
 	{
 		-- 壓制火光
@@ -93,21 +58,49 @@ local mistakes = {
 		spell = 355439,
 	},
 	{
+		-- 震擊地雷
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355476,
+	},
+	{
+		-- 致命武力
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355487,
+	},
+	{
+		-- 震光屏障
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355502,
+	},
+	{
+		-- 鎮壓猛襲
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355638,
+	},
+	{
+		-- 干擾手榴彈
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 355903,
+	},
+	{
+		-- 光束接合者
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 356011,
+	},
+	{
+		-- 腐爛的食物
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 356482,
+	},
+	{
+		-- 聖光裂片撤退
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 357019,
+	},
+	{
 		-- 不許進入！
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 357799,
-	},
-	-- [4] 郵務主管
-	{
-		-- 灑出的液體
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 346329,
-	},
-	-- [5] 索阿茲米
-	{
-		-- 殊離之環
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 347481,
 	},
 }
 
