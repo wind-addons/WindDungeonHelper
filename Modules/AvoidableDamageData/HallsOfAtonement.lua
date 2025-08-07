1bbfa38a3ecb7199225e858a0cd3b13210453e5a
Zhou Fang <fang2hou@outlook.com>
2025-08-07 21:36:40 +0900

    style: sort with id

--- Modules/AvoidableDamageData/HallsOfAtonement.lua
+++ Modules/AvoidableDamageData/HallsOfAtonement.lua
@@ -2,11 +2,46 @@ local W, F, L, P = unpack(select(2, ...))
 local AD = W:GetModule("AvoidableDamage")
 
 local mistakes = {
-	-- 小怪
 	{
-		-- 致命推進 (墮落的暗刃兵)
+		-- 血腥洪流 (艾可隆)
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 319702,
+	},
+	{
+		-- 拋擲殘骸 (哈奇厄斯)
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 322945,
+	},
+	{
+		-- 玻璃裂片 (哈奇厄斯)
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 323001,
+		playerIsNotTank = true,
+	},
+	{
+		-- 念力碰撞
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 323126,
+	},
+	{
+		-- 釋放磨難 (宮務大臣)
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 323236,
+	},
+	{
+		-- 折射罪光 (哈奇厄斯)
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 324044,
+	},
+	{
+		-- 致命推進
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 325523,
+	},
+	{
+		-- 急速射擊
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 325799,
 	},
 	{
 		-- 罪孽震盪 (哈奇厄斯裂片)
@@ -14,71 +49,31 @@ local mistakes = {
 		spell = 326440,
 	},
 	{
-		-- 急速射擊 (墮落的馴犬者)
+		-- 強力揮擊 (石源魔斬擊者)
 		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 325799,
+		spell = 326997,
+		playerIsNotTank = true,
+	},
+	{
+		-- 爆發折磨 (宮務大臣)
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 327885,
+	},
+	{
+		-- 念力猛襲 (宮務大臣)
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 329113,
+	},
+	{
+		-- 靈魄泉 (靈魄泉, 至高判決者阿利茲)
+		type = AD.MISTAKE.SPELL_DAMAGE,
+		spell = 338013,
 	},
 	{
 		-- 岩石之息
 		type = AD.MISTAKE.SPELL_DAMAGE,
 		spell = 346866,
 		playerIsNotTank = true,
-	},
-	{
-		-- 強力揮擊 (石源魔斬擊者)
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 326997,
-		playerIsNotTank = true,
-	},
-	-- [1]『罪污巨人』哈奇厄斯
-	{
-		-- 拋擲殘骸
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 322945,
-	},
-	{
-		-- 玻璃裂片
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 323001,
-		playerIsNotTank = true,
-	},
-	{
-		-- 折射罪光
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 324044,
-	},
-	-- [2] 艾可隆
-	{
-		-- 血腥洪流
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 319702,
-	},
-	-- [3] 至高判決者阿利茲
-	{
-		-- 靈魂泉
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 338013,
-	},
-	-- [4] 宮務大臣
-	{
-		-- 念力碰撞
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 323126,
-	},
-	{
-		-- 念力猛襲
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 329113,
-	},
-	{
-		-- 釋放磨難
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 323236,
-	},
-	{
-		-- 爆發折磨
-		type = AD.MISTAKE.SPELL_DAMAGE,
-		spell = 327885,
 	},
 }
 
